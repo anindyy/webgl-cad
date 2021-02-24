bff2cee589ea150eb18f9fdaac0180a0a6b9f1fe
Jon Felix Germinian <13518025@std.stei.itb.ac.id>
2021-02-24 15:51:17 +0700

    tried making triangles

--- main.js
+++ main.js
@@ -2,11 +2,12 @@
 
 main();
 
+
 function main() {
     const canvas = document.querySelector("#glCanvas");
     const gl = canvas.getContext("webgl");
 
-    if (gl === null) {
+    if (!gl) {
         alert("Unable to initialize WebGL.");
         return;
     }
@@ -33,7 +34,8 @@ function main() {
         program: shaderProgram,
         coord: gl.getAttribLocation(shaderProgram, "coordinates")
     }
-    drawScene(gl, programInfo, buffers);
+    // drawPoints(gl, programInfo, buffers, 3);
+    drawTriangles(gl, programInfo, buffers, 3);
 }
 
 function initShaderProgram(gl, vsSource, fsSource) {
@@ -93,7 +95,7 @@ function initBuffers(gl) {
     };
 }
 
-function drawScene(gl, programInfo, buffers) {
+function drawPoints(gl, programInfo, buffers, size) {
     // bind vertex buffer object
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
     // point an attribute to currently bound VBO
@@ -108,5 +110,23 @@ function drawScene(gl, programInfo, buffers) {
     gl.enable(gl.DEPTH_TEST);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.viewport(0, 0, programInfo.canvas.width, programInfo.canvas.height);
-    gl.drawArrays(gl.POINTS, 0, 3);
+    gl.drawArrays(gl.POINTS, 0, size);
+}
+
+function drawTriangles(gl, programInfo, buffers, size) {
+    // bind vertex buffer object
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    // point an attribute to currently bound VBO
+    gl.vertexAttribPointer(programInfo.coord, 3, gl.FLOAT, false, 0, 0);
+    // enable attribute
+    gl.enableVertexAttribArray(programInfo.coord);
+
+    gl.useProgram(programInfo.program);
+
+    // drawing on the canvas
+    gl.clearColor(0.5, 0.5, 0.5, 0.9);
+    gl.enable(gl.DEPTH_TEST);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.viewport(0, 0, programInfo.canvas.width, programInfo.canvas.height);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, size);
 }
